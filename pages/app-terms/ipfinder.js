b92e6e584b8fc9e576098364146a9144f6a6d20d
Francesco Alterio <francescoalteriog@gmail.com>
2022-08-16 16:45:06 -0400

    add app terms

---
+++ pages/app-terms/ipfinder.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Head from "next/head";
+import styles from "../../styles/Terms.module.css";
+
+function pokebook() {
+  return (
+    <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+
+        <title>Fran Studios | Ip Finder Terms</title>
+        <meta property="og:title" content="Fran Studios | Ip Finder Terms" />
+
+        <meta name="description" content="Ip Finder Terms and Conditions" />
+        <meta
+          property="og:description"
+          content="Ip Finder Terms and Conditions"
+        />
+
+        <meta property="og:url" content="https://franstudios.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://franstudios.vercel.app/logo.png"
+        />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1>Privacy policies</h1>
+        <ul>
+          <li className={styles.li}>
+            Ip Finder does not collect any personal data from users.
+          </li>
+          <li className={styles.li}>Ip Finder does not log IP addresses.</li>
+          <li className={styles.li}>
+            Ip Finder does not access the contact data of the device.
+          </li>
+          <li className={styles.li}>
+            Ip Finder does not access users email accounts.
+          </li>
+          <li className={styles.li}>
+            Ip Finder does not save data related to the device.
+          </li>
+          <li className={styles.li}>
+            Fran Studios, not Google, is solely responsible for the content of
+            Fran Studios.
+          </li>
+          <li className={styles.li}>
+            For more information about this privacy policy, you can contact
+            francescoalteriog@gmail.com
+          </li>
+        </ul>
+      </main>
+    </>
+  );
+}
+
+export default pokebook;
